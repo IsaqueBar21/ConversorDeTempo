581382c70191bed314536ffee8c0feedf67c142b
Isaque Barbosa da Silva <isaque_barbosa@jabil.com>
2023-10-16 08:31:16 -0300

    primeiro commit

---
+++ ConversorDeTempo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class URI {
+
+    static void Main(string[] args)  {
+
+        Console.WriteLine("Insira o valor em Segundos abaixo: ");
+        int N = int.Parse(Console.ReadLine());
+        int h, m, s;
+
+        h = N / 3600;
+        m = (N % 3600) / 60;
+        s = (N % 3600) % 60;
+
+        Console.WriteLine("Valor em Horas + Minutos + Segundos");
+        Console.WriteLine(h + ":" + m + ":" + s);
+
+    }
+
+}
